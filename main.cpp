b90acf79beaae9255d2cfee038b6cd76e92e62fb
lavinialazzeroni <118049473+lavinialazzeroni@users.noreply.github.com>
2022-11-27 21:10:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-   return 0;
+int main()
+{
+    int n=0, a=1, b=1, c=0;
+    cin>>n;
+    cout<<a<<endl<<b<<endl;
+    for (int i=0; i<=n; i++){
+        c = a+b;
+        a = b;
+        b = c;
+    cout<<c<<endl;
+    }
+    return 0;
 }
+
+
